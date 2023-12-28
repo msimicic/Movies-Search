a76f0f40cf920345fdc7f354f19be301025af808
Matej <matej_simicic@hotmail.com>
2023-12-28 21:13:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import SearchBar from "./Components/SearchBar";
+import SearchResultsList from "./Components/SearchResultsList";
+import MovieScreen from "./Components/MovieScreen";
 
 function App() {
+  const [results, setResults] = useState([]);
+  const [pickedMovie, setPickedMovie] = useState();
+  const [input, setInput] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-bar-container">
+        <SearchBar setResults={setResults} input={input} setInput={setInput} />
+        <SearchResultsList
+          results={results}
+          setPickedMovie={setPickedMovie}
+          setResults={setResults}
+          setInput={setInput}
+        />
+      </div>
+      {pickedMovie && (
+        <div className="movie-container">
+          <MovieScreen pickedMovie={pickedMovie} />
+        </div>
+      )}
     </div>
   );
 }
